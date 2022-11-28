d294edef381645aeec7abd64f14f3dbafa2f9a2d
Đặng Công Tấn <congtan2503@gmail.com>
2022-11-28 19:04:43 +0700

    Update

--- src/app/share/share-routing.module.ts
+++ src/app/share/share-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {IndexComponent} from './home/index/index.component';
 
 
 const routes: Routes = [
-  {path: '', component: IndexComponent}
 ];
 
 @NgModule({
